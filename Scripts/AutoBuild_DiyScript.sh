1c0d83334da87ebcba460c946e8135f961ccf645
onwingslnz <59407469+onwingslnz@users.noreply.github.com>
2021-03-16 17:28:28 +0800

    Update AutoBuild_DiyScript.sh

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -43,6 +43,7 @@ Diy-Part1() {
 	ExtraPackages svn lean luci-app-eqos https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t
 	ExtraPackages git other luci-app-bearDropper https://github.com/NateLol
 	ExtraPackages git other luci-app-onliner https://github.com/rufengsuixing
+	ExtraPackages git jd_openwrt_script jd_openwrt_script https://github.com/ITdesk01/jd_openwrt_script
 }
 
 Diy-Part2() {
